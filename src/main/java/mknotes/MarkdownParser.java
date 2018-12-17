3efdc46e466821015f67ef56fd0d1dd49c5dce2e
gregoryalary <gregalary26@gmail.com>
2018-12-18 00:16:26 +0100

    #1 Center and fludiy images

--- src/main/java/mknotes/MarkdownParser.java
+++ src/main/java/mknotes/MarkdownParser.java
@@ -1,16 +1,30 @@
 package mknotes;
 
 import org.apache.commons.io.FileUtils;
+import org.commonmark.node.Image;
 import org.commonmark.node.Node;
 import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.AttributeProvider;
+import org.commonmark.renderer.html.AttributeProviderContext;
+import org.commonmark.renderer.html.AttributeProviderFactory;
 import org.commonmark.renderer.html.HtmlRenderer;
 
 import java.io.File;
+import java.util.Map;
 
 /**
  * Render the site pages from the markdown sources
  */
 public final class MarkdownParser {
+
+    static class ImageAttributeProvider implements AttributeProvider {
+        @Override
+        public void setAttributes(Node node, String tagName, Map<String, String> attributes) {
+            if (node instanceof Image) {
+                attributes.put("class", "img-fluid mx-auto d-block");
+            }
+        }
+    }
 
     private MarkdownParser() { }
 
@@ -34,7 +48,12 @@ public final class MarkdownParser {
     public static String parseFile(String file) throws Exception {
         Parser parser = Parser.builder().build();
         Node document = parser.parse(getFileContent(file));
-        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        HtmlRenderer renderer = HtmlRenderer.builder()
+                .attributeProviderFactory(new AttributeProviderFactory() {
+                    public AttributeProvider create(AttributeProviderContext context) {
+                        return new ImageAttributeProvider();
+                    }
+                }).build();
         return renderer.render(document);
     }
 
