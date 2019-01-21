40bed0546886c12ded1415771c710bc4fa1daba8
Gregory <gregalary26@gmail.com>
2019-01-21 16:13:04 +0100

    close #11 Order sidebar alphabetically

--- src/main/java/mknotes/sidebar/SidebarNode.java
+++ src/main/java/mknotes/sidebar/SidebarNode.java
@@ -1,19 +1,22 @@
 package mknotes.sidebar;
 
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.*;
 
 public abstract class SidebarNode {
 
     /** Containg the child of the node <Title, Object> */
-    protected HashMap<String, SidebarNode> subtrees;
+    protected Map<String, SidebarNode> subtrees;
 
     protected String title;
 
     protected String href;
 
     public SidebarNode(String title, String href) {
-        this.subtrees = new HashMap<>();
+        this.subtrees = new TreeMap<String, SidebarNode>(new Comparator<String>() {
+            public int compare(String o1, String o2) {
+                return o1.toLowerCase().compareTo(o2.toLowerCase());
+            }
+        }); // ordered sidebar
         this.title = title;
         this.href = href;
     }
